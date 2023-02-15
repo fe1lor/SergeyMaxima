1737bbb34fcba29e4b881a905e0521d03968754a
fe1lor <roks.budy@gmail.com>
2023-02-15 15:14:48 +0500

    new

--- SergeyMaxima/src/Arrays/Main.java
+++ SergeyMaxima/src/Arrays/Main.java
@@ -2,9 +2,14 @@ package Arrays;
 
 public class Main extends Player {
     public static void main(String[] args) {
+        Ship shipOne = new Ship("Odnopalubnic", 1);
+        System.out.println(shipOne.getShipName());
+        System.out.println(shipOne.getShipSize());
+        Ship shipTwo = new Ship("Dvuhpalubnilk", 2);
+        System.out.println(shipTwo.getShipName());
+        System.out.println(shipTwo.getShipSize());
+        Ship shipOnlyNameOne = new Ship("Odnopalubnic");
+        System.out.println(shipOnlyNameOne.getShipName());
 
-
-
-
-}
+    }
 }
--- SergeyMaxima/src/Arrays/Ship.java
+++ SergeyMaxima/src/Arrays/Ship.java
@@ -1,4 +1,27 @@
 package Arrays;
 
 public class Ship {
+    int size = 0;
+    String name = "new";
+
+    public Ship(String name, int size) {
+        this.size = size;
+        this.name = name;
+    }
+
+    public Ship(String name) {
+        this.name = name;
+    }
+    public Ship(int size) {
+        this.size = size;
+         }
+
+    public String getShipName() {
+        return name;
+    }
+
+    public int getShipSize() {
+        return size;
+    }
+
 }
